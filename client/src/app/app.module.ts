fa34f76db95b0bc9af065cf9a6cc1e1bcc2d731c
Mihajilo Mrsevic <mmrsevic98@gmail.com>
2021-05-26 18:37:40 +0200

    End of section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,10 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { TestErrorComponent } from './test-error/test-error.component';
+import {ErrorInterceptor} from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TestErrorComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,9 @@ import { HomeModule } from './home/home.module';
     CoreModule,
     HomeModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {ToastrModule} from 'ngx-toastr';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
     RouterModule,
+    ToastrModule.forRoot({
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true,
+      }),
   ],
   exports: [NavBarComponent],
 })
